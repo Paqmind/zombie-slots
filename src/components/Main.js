c56942266bda2cfefac4bb79942b86bb1d862a0f
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2017-12-21 00:55:59 +0300

    slots added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,95 @@
-import React from 'react'
+import React, { Component } from 'react'
+import '../styles/styles.css'
 
-const Main = () => {
-  return (<div>Hi there</div>)
+class Main extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    var icons = [
+      'apple','bananas','cherry','grapes','orange','pineapple','strawberry','watermelon'
+    ];
+
+    var slots = document.querySelector('.slots');
+    var cols = document.querySelectorAll('.col');
+    for(var i in cols) {
+      if (!cols.hasOwnProperty(i))
+        continue;
+      var col = cols[i];
+      var str = '';
+      var firstThree = '';
+      for(var x = 0; x < 30; x++) {
+        var part = '<svg class="icon"><use xlink:href="#icon-'+icons[Math.floor(Math.random()*icons.length)]+'"></use></svg>';
+        str += part
+        if (x < 3) firstThree += part;
+      }
+      col.innerHTML = str+firstThree;
+    }
+
+    document.querySelector('input').focus();
+  }
+
+  spin = (elem) => {
+    if(!elem) return
+  elem.setAttribute('disabled', true);
+  slots.classList.toggle('spinning', true);
+  window.setTimeout(function() {
+    for(var i in cols) {
+      if (!cols.hasOwnProperty(i))
+        continue;
+      var col = cols[i];
+
+      var icons = [
+        'apple','bananas','cherry','grapes','orange','pineapple','strawberry','watermelon'
+      ];
+      var results = [
+        icons[Math.floor(Math.random()*icons.length)],
+        icons[Math.floor(Math.random()*icons.length)],
+        icons[Math.floor(Math.random()*icons.length)]
+
+      ];
+      console.log(results[1])
+      var icons = col.querySelectorAll('.icon use');
+      for(var x = 0; x < 3; x++) {
+        icons[x].setAttribute('xlink:href', '#icon-'+results[x]);
+        icons[(icons.length-3)+x].setAttribute('xlink:href', '#icon-'+results[x]);
+      }
+    }
+  }, 1500);
+
+  window.setTimeout(function() {
+    slots.classList.toggle('spinning', false);
+    elem.removeAttribute('disabled');
+    elem.focus();
+  }.bind(elem), 3500);
+}
+
+
+  render() {
+    return (
+    <div className="slots">
+      <div className="window">
+        <div className="outer-col">
+          <div className="col"></div>
+        </div>
+        <div className="outer-col">
+          <div className="col"></div>
+        </div>
+        <div className="outer-col">
+          <div className="col"></div>
+        </div>
+        <div className="outer-col">
+          <div className="col"></div>
+        </div>
+        <div className="outer-col">
+          <div className="col"></div>
+        </div>
+      </div>
+      <input id="input" type="button" onClick={console.log(this)} value="spin" />
+    </div>
+    )
+  }
 }
 
 export default Main
