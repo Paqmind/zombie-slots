561e9abf7ec424ca693bd03a944fa088293aa4e9
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2017-12-24 13:38:06 +0300

    html classes changed, spin mechanism improoved

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import '../styles/svg_styles.css'
 import '../styles/styles.css'
 
 class Main extends Component {
@@ -7,45 +8,41 @@ class Main extends Component {
   }
 
   componentDidMount() {
-    let icons = [
-      'apple','bananas','cherry','grapes','orange','pineapple','strawberry','watermelon'
-    ];
+    let icons = ['one','two','three','four','five','six','seven','eight'],
+        slots = document.querySelector('.slots'),
+        cols = document.querySelectorAll('.col')
 
-    let slots = document.querySelector('.slots');
-    let cols = document.querySelectorAll('.col');
     for(let i in cols) {
-      if (!cols.hasOwnProperty(i))
-        continue;
-      let col = cols[i];
-      let str = '';
-      let firstThree = '';
-      for(let x = 0; x < 30; x++) {
-        let part = '<svg class="icon"><use xlink:href="#icon-'+icons[Math.floor(Math.random()*icons.length)]+'"></use></svg>';
-        str += part
-        if (x < 3) firstThree += part;
-      }
-      col.innerHTML = str+firstThree;
-    }
+      if (!cols.hasOwnProperty(i)) continue
+      let col = cols[i],
+          str = '',
+          firstThree = ''
 
-    document.querySelector('input').focus();
+      for(let x = 0; x < 30; x++) {
+        let part = '<svg class="zombies"><use xlink:href="#zombie-'+icons[Math.floor(Math.random()*icons.length)]+'"></use></svg>'
+        str += part
+        if (x < 3) firstThree += part
+      }
+      col.innerHTML = str+firstThree
+    }
+    document.querySelector('input').focus()
   }
 
   spin = () => {
-    let slots = document.querySelector('.slots')
-    let cols = document.querySelectorAll('.col')
-    let button = document.getElementById('input')
-    button.setAttribute('disabled', true)
+    let slots = document.querySelector('.slots'),
+        cols = document.querySelectorAll('.col'),
+        button = document.getElementById('input')
 
+    button.setAttribute('disabled', true)
     slots.classList.toggle('spinning', true)
 
-    setTimeout(function () {
+    setTimeout(() => {
       for (let i in cols) {
         if (!cols.hasOwnProperty(i)) continue
+
         let col = cols[i]
 
-        let iconsNames = [
-          'apple','bananas','cherry','grapes','orange','pineapple','strawberry','watermelon'
-        ]
+        let iconsNames = ['one','two','three','four','five','six','seven','eight']
 
         let results = [
           iconsNames[Math.floor(Math.random()*iconsNames.length)],
@@ -53,46 +50,33 @@ class Main extends Component {
           iconsNames[Math.floor(Math.random()*iconsNames.length)]
         ]
 
-        let icons = col.querySelectorAll('.icon use');
+        let icons = col.querySelectorAll('.zombies use')
         for(let x = 0; x < 3; x++) {
-          icons[x].setAttribute('xlink:href', '#icon-'+results[x]);
-          icons[(icons.length-3)+x].setAttribute('xlink:href', '#icon-'+results[x]);
+          icons[x].setAttribute('xlink:href', '#zombie-'+results[x])
+          icons[(icons.length-3)+x].setAttribute('xlink:href', '#zombie-'+results[x])
         }
       }
     }, 1500)
 
-    setTimeout(function () {
+    setTimeout(() => {
       slots.classList.toggle('spinning', false)
       button.removeAttribute('disabled')
+      document.querySelector('input').focus()
     }, 3500
     )
   }
 
   render() {
-    return (
-      <div>
-    <div className="slots">
+    return <div className="slots">
       <div className="window">
-        <div className="outer-col">
-          <div className="col"></div>
-        </div>
-        <div className="outer-col">
-          <div className="col"></div>
-        </div>
-        <div className="outer-col">
-          <div className="col"></div>
-        </div>
-        <div className="outer-col">
-          <div className="col"></div>
-        </div>
-        <div className="outer-col">
-          <div className="col"></div>
-        </div>
+        <div className="outer-col"><div className="col"></div></div>
+        <div className="outer-col"><div className="col"></div></div>
+        <div className="outer-col"><div className="col"></div></div>
+        <div className="outer-col"><div className="col"></div></div>
+        <div className="outer-col"><div className="col"></div></div>
       </div>
+      <input id="input" type="button" onClick={this.spin} value="spin" />
     </div>
-        <input id="input" type="button" onClick={this.spin} value="spin" />
-      </div>
-    )
   }
 }
 
