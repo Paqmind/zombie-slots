9486f49a839fdefe6cc93ed4526903451d5ac8aa
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2018-02-13 20:27:35 +0300

    coins counter created

--- src/components/slots/Slots.js
+++ src/components/slots/Slots.js
@@ -4,25 +4,30 @@ import svgClassNames from './svgClassNames'
 
 const Slots = (props) => {
   let cols = props.state.cols.slice(),
-      icons = props.state.winningIcons
+    icons = props.state.winningIcons
 
   for (let col in cols) {
     cols[col] = cols[col].concat(colsTemplate.concat(cols[col]))
   }
 
-  return <div className="wrapper">{
-    cols.map((col, i) => {
-      return <div className="outer-col" key={i}>
-        <div className="col">{
-          col.map((value, i) => {
-            return <svg className={svgClassNames(icons)} key={i}>
-              <use xlinkHref={`#zombie-${value}`}></use>
-            </svg>
-          })
-        }</div>
-      </div>
-    })
-  }</div>
+  return <div className="wrapper">
+    <div className="show-coins-counter">
+      <div className="coins-amount">200</div>
+    </div>
+    {
+      cols.map((col, i) => {
+        return <div className="outer-col" key={i}>
+          <div className="col">{
+            col.map((value, i) => {
+              return <svg className={svgClassNames(icons)} key={i}>
+                <use xlinkHref={`#zombie-${value}`}></use>
+              </svg>
+            })
+          }</div>
+        </div>
+      })
+    }
+  </div>
 }
 
 export default Slots
