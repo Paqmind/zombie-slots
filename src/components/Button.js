813d6ea3b624250b5322472766c033304010d14c
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2018-03-02 22:26:43 +0300

    Button component styles changed

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,6 +4,7 @@ const Button = (props) => {
 
   return <input id="input"
                 className={props.btnDisabled ? "btn-disabled" : "btn-active"}
+                value="SPIN"
                 type="button"
                 onClick={props.spin}
                 disabled={props.btnDisabled}
